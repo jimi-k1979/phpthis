ac555dba461a427c1e350cd372dda8f5bb74cfd3
James Kerslake <james@myconid.co.uk>
2018-08-30 20:15:22 +0100

    first php commit

---
+++ app/Person/Person.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PhpThis\Person;
+
+class Person {
+  
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once('vendor/autoload.php');
+
+//phpinfo();
+
+// use PhpThis\Menu
+use PhpThis\Person\Person;
+
+$person = new Person();
