185c61951ba90973ddda09065c059d461fc88283
James Kerslake <james@myconid.co.uk>
2018-09-21 12:30:54 +0100

    User example 1

--- app/Person/User.php
+++ app/Person/User.php
@@ -2,7 +2,35 @@
 namespace PhpThis\Person;
 
 class User {
-
+  private $firstname = 'John';
+  private $lastname = 'Doe';
+  private $email = 'johndoe@domain.com';
+  
   public function __construct() {
+    // not implemented
+  }
+  
+  public function setFirstName($firstname) {
+    $this->firstname = $firstname;
+  }
+  
+  public function getFirstName() {
+    return $this->firstname;
+  }
+  
+  public function setLastName($lastname) {
+    $this->lastname = $lastname;
+  }
+  
+  public function getLastName() {
+    return $this->lastname;
+  }
+  
+  public function setEmail($email) {
+    $this->email = $email;
+  }
+  
+  public function getEmail() {
+    return $this->email;
   }
 }
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require_once('vendor/autoload.php');
 
-use PhpThis\Animal\Cow;
+use PhpThis\Person\User;
 
-$milky = new Cow();
-echo $milky->created;
+$user = new User();
+$user->setFirstName('John');
+$user->setLastName('Doe');
+$user->setEmail('john@email.com');
+
+echo 'First Name: ' . $user->getFirstName() . "\nLast Name: " . $user->getLastName() . "\nEmail: " . $user->getEmail() . "\n";
+
