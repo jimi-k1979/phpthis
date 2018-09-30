03828357c0f9c11d97f793de3ebfaa64b9e2003f
James Kerslake <james@myconid.co.uk>
2018-09-30 11:41:17 +0100

    game aside - enemy classes

---
+++ app/Animal/GrizzlyBear.php
@@ -0,0 +1,9 @@
+<?php
+namespace PhpThis\Animal;
+
+class GrizzlyBear extends Animal {
+  public function growlAtYou() {}
+  public function chaseYou() {}
+  public function pounceOnYourCar() {}
+  public function eatYou() {}  
+}
---
+++ app/Person/Cop.php
@@ -0,0 +1,8 @@
+<?php
+namespace PhpThis\Person;
+
+class Cop extends Person {
+  public function followYou() {}
+  public function pullYouOver() {}
+  public function arrestYou() {}
+}
