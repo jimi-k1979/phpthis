3e0a0e092696b56ef6c10c38d8cf786ed138c663
James Kerslake <james@myconid.co.uk>
2018-10-06 20:19:46 +0100

    refactor MenuItem

--- app/Menu/MenuItem.php
+++ app/Menu/MenuItem.php
@@ -2,58 +2,20 @@
 namespace PhpThis\Menu;
 
 class MenuItem {
-  private $menuItemId;
-  private $itemName;
-  private $description;
-  private $price;
-  private $servingSize;
-  private $picture;
+  // this is considered bad form now - difficult to run unit tests on
   
-  public function setMenuItemId(int $menuItemId): void {
-    $this->menuItemId = $menuItemId;
+  // constructor (not implemented)
+  public function __construct(){}
+  
+  // set undeclared property
+  public function __set($property, $value) {
+    $this->$property = $value;
   }
   
-  public function getMenuItemId(): int {
-    return $this->menuItemId;
-  }
-  
-  public function setPrice(float $price): void {
-    $this->price = $price;
-  }
-  
-  public function getPrice(): float {
-    return $this->price;
-  }
-  
-  public function setPicture(string $picture): void {
-    $this->picture = $picture;
-  }
-  
-  public function getPicture(): string {
-    return $this->picture;
-  }
-  
-  public function setItemName(string $itemName): void {
-    $this->itemName = $itemName;
-  }
-  
-  public function getItemName(): string {
-    return $this->itemName;
-  }
-  
-  public function setDescription(string $description): void {
-    $this->description = $description;
-  }
-  
-  public function getDescription(): string {
-    return $this->description;
-  }
-  
-  public function setServingSize(string $servingSize):void {
-    $this->servingSize = $servingSize;
-  }
-  
-  public function getServingSize(): string {
-    return $this->servingSize;
+  // get defined property
+  public function __get($property) {
+    if (isset($this->$proprty)) {
+      $this->$property;
+    }
   }
 }
