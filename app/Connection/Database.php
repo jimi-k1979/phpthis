37e6964fb35dd2feb78091482e724948d8989314
James Kerslake <james@myconid.co.uk>
2018-10-06 19:55:24 +0100

    mysqli oo style

--- app/Connection/Database.php
+++ app/Connection/Database.php
@@ -11,14 +11,14 @@ class Database {
     $databasePassword = $GLOBALS['configuration']['pass'];
     $databasePort = $GLOBALS['configuration']['port'];
     
-    $this->connection = mysqli_connect($serverName, $databaseUser, $databasePassword, $databaseName, $databasePort);
-    mysqli_set_charset($this->connection, 'utf8mb4');
-    if (!$this->connection) {
+    $this->connection = new \mysqli($serverName, $databaseUser, $databasePassword, $databaseName, $databasePort);
+    if (!$this->connection->connect_error) {
       throw new \Exception('Cannot connect to the database');
-    }
+    }    
+    $this->connection->set_charset('utf8mb4');
   }
   
-  public static function connect() {
+  public static function connect(): \mysqli {
     static $database = null;
     if (!isset($database)) {
       $database = new Database();
@@ -26,22 +26,22 @@ class Database {
     return $database->connection;
   }
   
-  public static function reader(string $query, $connnection) {
-    $cursor = mysqli_query($connnection, $query);
+  public static function reader(string $query): \mysqli_result {
+    $cursor = $this->connection->query($query);
     return $cursor;
   }
   
-  public static function read($cursor) {
-    return mysqli_fetch_assoc($cursor);
+  public static function read(\mysqli_result $cursor): ?array {
+    return $cursor->fetch_assoc();
   }
   
-  public static function query($query, $connection) {
-    $result = mysqli_query($connection, $query);
-    return mysqli_num_rows($result);
+  public static function query(string $query): ?int {
+    $result = $this->connection->mysqli_query($query);
+    return $result->num_rows;
   }
   
-  public static function insertOrUpdate($query, $connection) {
-    $result = mysqli_query($connection, $query);
-    return intval(mysqli_insert_id($connection));
+  public static function insertOrUpdate(string $query): int {
+    $this->connection->query($query);
+    return (int) $this->connection->insert_id;
   }
 }
